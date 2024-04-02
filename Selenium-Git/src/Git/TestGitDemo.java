9e2b3e1e73d39aabc52fd20cceb1fd9f7d47b683
gadhepooja11 <165790746+gadhepooja11@users.noreply.github.com>
2024-04-02 23:47:45 +0530

    by git

--- Selenium-Git/src/Git/TestGitDemo.java
+++ Selenium-Git/src/Git/TestGitDemo.java
@@ -10,7 +10,10 @@ public class TestGitDemo
 	{
 		System.out.println("This is m2 of gitDemo");
 	}
-	
+	public void m3()
+	{
+	  System.out.println("This is m3 of gitDemo");	
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("This Is Git Demo");
